bf7b1affe0541642d353d4608b4c4cd33adefd77
Sami Halim <samihalim@gmail.com>
2020-11-23 15:21:28 +0100

    added desks and bookings controller actions

---
+++ app/controllers/desks_controller.rb
@@ -0,0 +1,54 @@
+class DesksController < ApplicationController
+  before_action :set_desk, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @desks = Desk.all
+  end
+
+  def show
+  end
+
+  def new
+    @desk = Desk.new
+  end
+
+  def edit
+  end
+
+  def create
+    @desk = Desk.new(desk_params)
+
+    if @desk.save!
+      redirect_to @desk, notice: 'A desk was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @desk.update(desk_params)
+      redirect_to @desk, notice: 'Desk was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def my_desks
+    currentuser.desks
+  end
+
+  def destroy
+    @desk.destroy
+    redirect_to desks_url, notice: 'Desk was successfully destroyed.'
+  end
+
+  private
+
+  def set_desk
+    @desk = Desk.find(params[:id])
+  end
+
+  def desk_params
+    params.require(:desk).permit(:price, :location, :seats, :start_date, :end_date)
+  end
+end
