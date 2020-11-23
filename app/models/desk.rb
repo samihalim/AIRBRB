ffca80fcc78c18da36dbfb69ddbd8a7a3f47fe79
Fabian Tewes <fabian.tewes@gmail.com>
2020-11-23 17:37:36 +0100

    new desk push

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -5,6 +5,7 @@ class Desk < ApplicationRecord
   validates :price, presence: true, allow_blank: false
   validates :location, presence: true, allow_blank: false
   validates :seats, presence: true, allow_blank: false
+  validates :title, presence: true, allow_blank: false
   validates :start_date, presence: true
   validates :end_date, presence: true
 end
