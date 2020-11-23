ffa515be05af0c52bd05c3c0e0ffb9e3e30bff2c
Sami Halim <samihalim@gmail.com>
2020-11-23 13:51:27 +0100

    model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :desks
+  has_many :bookings
+
+  validates :first_name, presence: true, allow_blank: false
+  validates :last_name, presence: true, allow_blank: false
+  validates :email, presence: true, uniqueness: true, allow_blank: false
 end
