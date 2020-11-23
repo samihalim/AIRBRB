fb59936b83b350c8cd9fa1c851063f90b8915fbe
Fabian Tewes <fabian.tewes@gmail.com>
2020-11-23 14:48:44 +0100

    added belongs to and has many

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
+  has_many :desks
+  has_many :bookings, through: :desks
 end
