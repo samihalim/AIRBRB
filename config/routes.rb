72a9e3ae15cd98f16cae72dd419b2c2b5573271c
Sami Halim <samihalim@gmail.com>
2020-11-23 15:34:34 +0100

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :desks, only: [:show, :new, :create, :destroy, :edit] do
-    resources :bookings, only: [:index, :new, :create]
+  resources :desks do
+    resources :bookings
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
