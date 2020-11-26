3aba19612f26633e4604b6f87d019124d285ec21
Fabian Tewes <fabian.tewes@gmail.com>
2020-11-26 14:08:32 +0100

    remove time from flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,3 +1,2 @@
 import flatpickr from "flatpickr";
-import 'flatpickr/dist/themes/airbnb.css'
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+import 'flatpickr/dist/themes/airbnb.css';
